473b426a3022b6e3b9d0bffb7a245b5340fab2db
prem-12345 <premkpatil1997@gmail.com>
2021-10-01 12:27:02 +0530

    Added query to retrieve data from database

--- src/main/java/com/bridgelabz/employeepayrolljdbc/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrolljdbc/EmployeePayrollService.java
@@ -1,14 +1,14 @@
 package com.bridgelabz.employeepayrolljdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class EmployeePayrollService {
     public static void main(String[] args) {
 
-        //WELCOME TO EMPLOYEE PAYROLL SERVICE JDBC
+        //WELCOME TO EMPLOYEE PAYROLL SERVICE MYSQL JDBC
 
         //UC1 - CREATE EMPLOYEE PAYROLL DATABASE AND CONNECT JAVA PROGRAM TO DATABASE
         try {
@@ -20,9 +20,35 @@ public class EmployeePayrollService {
             Connection connection = DriverManager.getConnection(DB_URL, "root", "prem@1234567");
 
             //STEP 3 : EXECUTE SQL QUERY
-            String CREATE_TABLE_QUERY = "create table employee_payroll(ID int NOT NULL AUTO_INCREMENT UNIQUE,NAME varchar(150) NOT NULL,GENDER char(1) NOT NULL,PHONE_NUMBER int NOT NULL,ADDRESS varchar(150) NOT NULL,DEPARTMENT varchar(50) NOT NULL,SALARY double NOT NULL,BASIC_PAY double NOT NULL,DEDUCTIONS double NOT NULL,NET_PAY double NOT NULL,START_DATE varchar(50) NOT NULL)";
+            String CREATE_TABLE_QUERY = "create table employee_payroll(ID int NOT NULL AUTO_INCREMENT UNIQUE,NAME varchar(150) NOT NULL,GENDER varchar(1) NOT NULL,PHONE_NUMBER int NOT NULL,ADDRESS varchar(150) NOT NULL,DEPARTMENT varchar(50) NOT NULL,SALARY double NOT NULL,BASIC_PAY double NOT NULL,DEDUCTIONS double NOT NULL,NET_PAY double NOT NULL,START_DATE varchar(50) NOT NULL)";
+            String INSERT_QUERY = "insert into employee_payroll(ID,NAME,GENDER,PHONE_NUMBER,ADDRESS,DEPARTMENT,SALARY,BASIC_PAY,DEDUCTIONS,NET_PAY,START_DATE) values (1,'TERISSA','F',85965469,'GOA','SALES',10000.00,10000.00,2000.00,8000.00,'2020-06-01'),(2,'PREM','M',73875576,'NASHIK','HR',10000.00,10000.00,2000.00,8000.00,'2018-06-01')";
+
+            //UC2 - RETRIEVE THE EMPLOYEE PAYROLL DATA FROM DATABASE
+            String FETCH_RECORD_QUERY = "select * from employee_payroll ";
             Statement statement = connection.createStatement();
-            statement.execute(CREATE_TABLE_QUERY);
+            ResultSet resultSet = statement.executeQuery(FETCH_RECORD_QUERY);
+
+            List<Employee> employees = new ArrayList<>();
+            while (resultSet.next()){
+                Employee employee = new Employee();
+                employee.setId(resultSet.getInt("ID"));
+                employee.setName(resultSet.getString("NAME"));
+                employee.setGender(resultSet.getString("GENDER"));
+                employee.setPhoneNumber(resultSet.getInt("PHONE_NUMBER"));
+                employee.setAddress(resultSet.getString("ADDRESS"));
+                employee.setDepartment(resultSet.getString("DEPARTMENT"));
+                employee.setSalary(resultSet.getInt("SALARY"));
+                employee.setBasicPay(resultSet.getInt("BASIC_PAY"));
+                employee.setDeduction(resultSet.getInt("DEDUCTIONS"));
+                employee.setNetPay(resultSet.getInt("NET_PAY"));
+                employee.setStartDate(resultSet.getString("START_DATE"));
+                employees.add(employee);
+            }
+
+            for (Employee emp:employees){
+                System.out.println(emp);
+            }
+
             System.out.println("QUERY EXECUTED");
 
             //STEP 4 : CLOSE THE CONNECTION AND STATEMENT
@@ -35,4 +61,3 @@ public class EmployeePayrollService {
 
     }
 }
-
