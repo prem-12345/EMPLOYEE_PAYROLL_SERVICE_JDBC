3122885060a5aeaab6cad854a06cf7b367bb2358
prem-12345 <premkpatil1997@gmail.com>
2021-10-01 12:04:28 +0530

    Welcome to employee payroll service jdbc

---
+++ src/main/java/com/bridgelabz/employeepayrolljdbc/Employee.java
@@ -0,0 +1,121 @@
+package com.bridgelabz.employeepayrolljdbc;
+
+public class Employee {
+    int id;
+    String name;
+    String gender;
+    int phoneNumber;
+    String address;
+    String department;
+    double salary;
+    double basicPay;
+    double deduction;
+    double netPay;
+    String startDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getBasicPay() {
+        return basicPay;
+    }
+
+    public void setBasicPay(double basicPay) {
+        this.basicPay = basicPay;
+    }
+
+    public double getDeduction() {
+        return deduction;
+    }
+
+    public void setDeduction(double deduction) {
+        this.deduction = deduction;
+    }
+
+    public double getNetPay() {
+        return netPay;
+    }
+
+    public void setNetPay(double netPay) {
+        this.netPay = netPay;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender=" + gender +
+                ", phoneNumber=" + phoneNumber +
+                ", address='" + address + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                ", basicPay=" + basicPay +
+                ", deduction=" + deduction +
+                ", netPay=" + netPay +
+                ", startDate='" + startDate + '\'' +
+                '}';
+    }
+}
+
---
+++ src/main/java/com/bridgelabz/employeepayrolljdbc/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeepayrolljdbc;
+
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        //WELCOME TO EMPLOYEE PAYROLL SERVICE JDBC
+    }
+}
